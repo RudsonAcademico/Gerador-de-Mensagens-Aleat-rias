cfa445a223d169f1529263b7e7213218e655f504
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2025-02-21 13:07:20 +0000

    finalizar

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-// Array de frases motivacionais
-const frases = [
+const frase_e_piadas = [
     "Acredite em si mesmo e tudo será possível.",
     "O sucesso é a soma de pequenos esforços repetidos diariamente.",
     "Não tenha medo de errar, tenha medo de não tentar.",
     "Grandes coisas nunca vêm de zonas de conforto.",
     "A persistência realiza o impossível.",
-    "Cada novo dia é uma nova oportunidade para ser melhor."
+    "Cada novo dia é uma nova oportunidade para ser melhor.",
+    "Seja como um próton. Sempre seja positivo.",
+    "Por que o desenvolvedor faliu? Porque ele usou todo o seu cache.",
+    'Meus professores me disseram que eu nunca seria muito porque procrastino muito. Eu disse a eles: “Esperem para ver!”',
+    "Minha memória ficou tão ruim que realmente me fez perder o emprego. Ainda estou empregado. Só não consigo lembrar onde.",
+    "Como chamar um cão mágico? Um Labracadabrador.",
+    "Há quem diga que Mc Kevin foi melhor que Michael Jackson mas, pelo menos Michael Jackson ia até o chão e voltava",
+    "O que as pernas de um paraplégico tem em comum com o comunismo? Ambos não funcionam",
+    "Acabei de encontrar uma barata gay, ela estava saindo do armário"
 ];
 
-// Rota para obter uma frase aleatória
 app.get("/quote", (req, res) => {
-    const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
-    res.json({ mensagem: fraseAleatoria });
+    const frase = frase_e_piadas[Math.floor(Math.random() * frase_e_piadas.length)];
+    res.json({ mensagem: frase });
 });
 
-// Inicia o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}/quote`);
 });
